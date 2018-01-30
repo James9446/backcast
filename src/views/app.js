938f5dd73d28c9c6970da99993e6b5ff85dd9c28
James9446 <jameshreynolds@gmail.com>
2018-01-29 20:50:33 -0800

    BackCast connected to the DOM

--- src/views/app.js
+++ src/views/app.js
@@ -3,13 +3,15 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);  //this is the collection
+    this.render();
   },
 
 
   render: function() {
     this.$el.html(this.template());
-    return this;
+    //this.$el.html('<p>HEY THERE</p>');
+    return this.$el;
   },
 
   template: templateURL('src/templates/app.html')
